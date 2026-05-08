0e3693feade2bffcf2256688c003c60c22311d32
doableworks <tech.doable@gmail.com>
2026-05-08 18:19:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	async redirects() {
-		return [
-			{
-				source: "/:path*",
-				destination: "https://www.gdpartners.co.in/",
-				permanent: false,
-			},
-		];
-	},
 	images: {
 		domains: ["img.youtube.com"],
 	},
